9dd2cc9c156e5fd6a95a5221a95a1958dd1f2f0e
Максим Гринь <grinmaksim5@gmail.com>
2024-07-30 16:34:34 +0300

    Рефакторинг проекта.

--- ALM.Tests/OracleDbDispatcherTests.cs
+++ ALM.Tests/OracleDbDispatcherTests.cs
@@ -1,18 +1,11 @@
-﻿using ALM.Entities;
-using ALM.Logging;
+﻿using ALM.Logging;
 
 using System.Text.RegularExpressions;
-using System.Collections.Generic;
-using System.Reflection;
-using System.Text;
-using System.IO;
 using System;
 
 using NetTopologySuite.Geometries;
 
 using Oracle.ManagedDataAccess.Client;
-
-using Xunit.Sdk;
 using Xunit;
 
 using Moq;
@@ -73,152 +66,6 @@ namespace ALM.Tests
 
             var expected = "MULTILINESTRING((13833.932 23147.894,13834.375 23147.558))";
             Assert.Equal(expected, actual);
-        }
-    }
-    public class DatFileDataAttribute : DataAttribute
-    {
-        private readonly string _filePath;
-        private readonly string _horizon;
-        public DatFileDataAttribute(string filePath, string horizon)
-        {
-            _filePath = filePath;
-            _horizon = horizon;
-        }
-        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
-        {
-            if (testMethod == null) throw new ArgumentNullException(nameof(testMethod));
-
-            using (var reader = new StreamReader(_filePath))
-            {
-                yield return new object[] { reader.ReadLine(), _horizon };
-            }
-        }
-    }
-    internal static class TestUtils
-    {
-        public static string ConnectionString
-        {
-            get
-            {
-                var builder = new StringBuilder().Append("Data Source=");
-
-                using (var reader = new StreamReader("../db.ini"))
-                {
-                    builder
-                        .Append(reader.ReadLine()).Append(";User Id=")
-                        .Append(reader.ReadLine()).Append(";Password=")
-                        .Append(reader.ReadLine()).Append(";Connection Timeout=360");
-                }
-
-                return builder.ToString();
-            }
-        }
-        private static readonly OracleRepository _repository;
-        public static OracleRepository Repository => _repository ?? new OracleRepository(new OracleConnection(ConnectionString));
-    }
-    public interface IRepository
-    {
-        Primitive GetByGuid(string guid, string horizon = "200F");
-        string GetLongGeometry(Primitive primitive, string horizon = "200F");
-        IEnumerable<Primitive> GetAllFullPolylines(int limit = int.MaxValue);
-    }
-    public class OracleRepository : IRepository
-    {
-        private readonly OracleConnection _connection;
-        public OracleRepository(OracleConnection connection)
-        {
-            _connection = connection;
-            _connection.Open();
-        }
-        public Primitive GetByGuid(string guid, string horizon = "200F")
-        {
-            using (var command = new OracleCommand($"SELECT * FROM \"K{horizon}_TRANS_CLONE\" WHERE \"OBJECTGUID\" = '{guid}'", _connection))
-            {
-                using (var reader = command.ExecuteReader())
-                {
-                    if (reader.Read())
-                    {
-                        return new Primitive(
-                            reader.GetString(reader.GetOrdinal("GEOWKT")),
-                            reader.GetString(reader.GetOrdinal("DRAWJSON")),
-                            reader.GetString(reader.GetOrdinal("PARAMJSON")),
-                            "",
-                            10,
-                            null,
-                            null,
-                            1,
-                            Guid.Parse(guid)
-                        );
-                    }
-                }
-            }
-
-            throw new InvalidOperationException($"Не удалось найти элемент с guid \"{guid.ToString().ToUpper()}\"");
-        }
-        private bool TryGetLongGeometry(string geometry, string guid, string numbColumnName, string horizon, out string longGeometry)
-        {
-            var builder = new StringBuilder(10_000).Append(geometry);
-            var query = $"SELECT \"PAGE\" FROM \"K{horizon}_TRANS_CLONE_GEOWKT\" WHERE OBJECTGUID = '{guid}' ORDER BY \"";
-            try
-            {
-                using (var command = new OracleCommand(query + numbColumnName + "\"", _connection))
-                {
-                    using (var reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            builder.Append(reader.GetString(0));
-                        }
-                    }
-                }
-
-                longGeometry = builder.ToString();
-                return true;
-            }
-            catch (OracleException)
-            {
-                longGeometry = string.Empty;
-                return false;
-            }
-        }
-        public string GetLongGeometry(Primitive primitive, string horizon = "200F")
-        {
-            if (TryGetLongGeometry(primitive.Geometry, primitive.Guid, "NUMB", horizon, out string longGeometry))
-            {
-                return longGeometry;
-            }
-            else if (TryGetLongGeometry(primitive.Geometry, primitive.Guid, "NUMBER", horizon, out longGeometry))
-            {
-                return longGeometry;
-            }
-            else
-            {
-                throw new InvalidOperationException($"Не удалось прочитать длинную геометрию объекта \"{primitive.Guid}\"");
-            }
-        }
-        public IEnumerable<Primitive> GetAllFullPolylines(int limit = int.MaxValue)
-        {
-            var query = $"SELECT * FROM \"K200F_TRANS_CLONE\" WHERE \"GEOWKT\" NOT LIKE 'POINT((%' AND \"GEOWKT\" LIKE '%))' AND ROWNUM < {limit}";
-            using (var command = new OracleCommand(query, _connection))
-            {
-                using (var reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        yield return new Primitive(
-                            reader.GetString(reader.GetOrdinal("GEOWKT")),
-                            reader.GetString(reader.GetOrdinal("DRAWJSON")),
-                            reader.GetString(reader.GetOrdinal("PARAMJSON")),
-                            "",
-                            10,
-                            null,
-                            null,
-                            1,
-                            Guid.Parse(reader.GetString(reader.GetOrdinal("OBJECTGUID")))
-                        );
-                    }
-                }
-            }
         }
     }
 }
---
+++ ALM/Repository/OracleRepository.cs
@@ -0,0 +1,110 @@
+﻿using ALM.Entities;
+
+using System.Collections.Generic;
+using System.Text;
+using System;
+
+using Oracle.ManagedDataAccess.Client;
+
+namespace ALM
+{
+    public class OracleRepository : IRepository
+    {
+        private readonly OracleConnection _connection;
+        public OracleRepository(OracleConnection connection)
+        {
+            _connection = connection;
+            _connection.Open();
+        }
+        public Primitive GetByGuid(string guid, string horizon = "200F")
+        {
+            using (var command = new OracleCommand($"SELECT * FROM \"K{horizon}_TRANS_CLONE\" WHERE \"OBJECTGUID\" = '{guid}'", _connection))
+            {
+                using (var reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        return new Primitive(
+                            reader.GetString(reader.GetOrdinal("GEOWKT")),
+                            reader.GetString(reader.GetOrdinal("DRAWJSON")),
+                            reader.GetString(reader.GetOrdinal("PARAMJSON")),
+                            "",
+                            10,
+                            null,
+                            null,
+                            1,
+                            Guid.Parse(guid)
+                        );
+                    }
+                }
+            }
+
+            throw new InvalidOperationException($"Не удалось найти элемент с guid \"{guid.ToString().ToUpper()}\"");
+        }
+        private bool TryGetLongGeometry(string geometry, string guid, string numbColumnName, string horizon, out string longGeometry)
+        {
+            var builder = new StringBuilder(10_000).Append(geometry);
+            var query = $"SELECT \"PAGE\" FROM \"K{horizon}_TRANS_CLONE_GEOWKT\" WHERE OBJECTGUID = '{guid}' ORDER BY \"";
+            try
+            {
+                using (var command = new OracleCommand(query + numbColumnName + "\"", _connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            builder.Append(reader.GetString(0));
+                        }
+                    }
+                }
+
+                longGeometry = builder.ToString();
+                return true;
+            }
+            catch (OracleException)
+            {
+                longGeometry = string.Empty;
+                return false;
+            }
+        }
+        public string GetLongGeometry(Primitive primitive, string horizon = "200F")
+        {
+            if (TryGetLongGeometry(primitive.Geometry, primitive.Guid, "NUMB", horizon, out string longGeometry))
+            {
+                return longGeometry;
+            }
+            else if (TryGetLongGeometry(primitive.Geometry, primitive.Guid, "NUMBER", horizon, out longGeometry))
+            {
+                return longGeometry;
+            }
+            else
+            {
+                throw new InvalidOperationException($"Не удалось прочитать длинную геометрию объекта \"{primitive.Guid}\"");
+            }
+        }
+        public IEnumerable<Primitive> GetAllFullPolylines(int limit = int.MaxValue)
+        {
+            var query = $"SELECT * FROM \"K200F_TRANS_CLONE\" WHERE \"GEOWKT\" NOT LIKE 'POINT((%' AND \"GEOWKT\" LIKE '%))' AND ROWNUM < {limit}";
+            using (var command = new OracleCommand(query, _connection))
+            {
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        yield return new Primitive(
+                            reader.GetString(reader.GetOrdinal("GEOWKT")),
+                            reader.GetString(reader.GetOrdinal("DRAWJSON")),
+                            reader.GetString(reader.GetOrdinal("PARAMJSON")),
+                            "",
+                            10,
+                            null,
+                            null,
+                            1,
+                            Guid.Parse(reader.GetString(reader.GetOrdinal("OBJECTGUID")))
+                        );
+                    }
+                }
+            }
+        }
+    }
+}
