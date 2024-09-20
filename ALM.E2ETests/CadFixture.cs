63a73e6ea19bb2dbda2b57937f6bc1c4a49c0e32
Максим Гринь <grinmaksim5@gmail.com>
2024-09-20 16:59:33 +0300

    Добавлен проект e2e тестов.

---
+++ ALM.E2ETests/CadFixture.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using System;
+
+namespace ALM.E2ETests
+{
+    public class CadFixture : IDisposable
+    {
+        public Process Process { get; }
+
+        public CadFixture()
+        {
+            const string cadAppPath = @"C:\Program Files\Autodesk\AutoCAD 2021\acad.exe";
+            const string cadAppArgs = "/b load_plugin.scr";
+
+            Process = Process.Start(cadAppPath, cadAppArgs);
+            Process.WaitForInputIdle();
+
+            Thread.Sleep(10_000);
+        }
+
+        public void Dispose()
+            => Process.Kill();
+    }
+}
---
+++ ALM.E2ETests/DrawCommandTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows.Automation;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+using Xunit;
+
+namespace ALM.E2ETests
+{
+    public class DrawCommandTests : IClassFixture<CadFixture>
+    {
+        private readonly Process _process;
+
+        public DrawCommandTests(CadFixture fixture)
+            =>  _process = fixture.Process;
+        
+        [Fact]
+        public void RunDrawCommand_ExecuteWithoutProblems()
+        {
+            AutomationElement mainWindow = AutomationElement.FromHandle(_process.MainWindowHandle);
+            
+            AutomationElement commandLine = mainWindow.FindFirst(TreeScope.Descendants,
+                new PropertyCondition(AutomationElement.AutomationIdProperty, "local:AutoCompleteEdit_1"));
+
+            if (commandLine != null)
+            {   
+                commandLine.SetFocus();
+
+                SendKeys.SendWait("MMP_DRAW");
+                SendKeys.SendWait("{ENTER}");
+            }
+
+            Thread.Sleep(1000);
+
+            var loginWindow = mainWindow.FindFirst(TreeScope.Descendants, new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Window));
+
+            // TODO: Добавить поиск кнопок действий
+        }
+    }
+}
