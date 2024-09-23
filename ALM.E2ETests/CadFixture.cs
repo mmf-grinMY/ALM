b53fd33b45f6744b99d1c89aa3a17d0068d88596
Максим Гринь <grinmaksim5@gmail.com>
2024-09-23 17:14:10 +0300

    Тесты расширены инициализацией окна подключения.

--- ALM.E2ETests/CadFixture.cs
+++ ALM.E2ETests/CadFixture.cs
@@ -4,21 +4,35 @@ using System;
 
 namespace ALM.E2ETests
 {
+    /// <summary>
+    /// Менеджер жизни тестируемого процесса
+    /// </summary>
     public class CadFixture : IDisposable
     {
+        /// <summary>
+        /// Тестируемый процесс
+        /// </summary>
         public Process Process { get; }
 
+        /// <summary>
+        /// Инициализация менеджера
+        /// </summary>
         public CadFixture()
         {
+            const int sleepTime = 12_000;
+
             const string cadAppPath = @"C:\Program Files\Autodesk\AutoCAD 2021\acad.exe";
             const string cadAppArgs = "/b load_plugin.scr";
 
             Process = Process.Start(cadAppPath, cadAppArgs);
             Process.WaitForInputIdle();
 
-            Thread.Sleep(10_000);
+            Thread.Sleep(sleepTime);
         }
 
+        /// <summary>
+        /// Закрытие тестируемого процесса
+        /// </summary>
         public void Dispose()
             => Process.Kill();
     }
--- ALM.E2ETests/DrawCommandTests.cs
+++ ALM.E2ETests/DrawCommandTests.cs
@@ -1,5 +1,4 @@
 ﻿using System.Windows.Automation;
-using System.Windows.Forms;
 using System.Diagnostics;
 using System.Threading;
 
@@ -7,34 +6,89 @@ using Xunit;
 
 namespace ALM.E2ETests
 {
+    /// <summary>
+    /// Класс содержит тест проверки работы команды MMP_DRAW
+    /// </summary>
+    /// <remarks>
+    /// При первом запуске тестов появится окно загрузки сборки с плагином, нужно выбрать пункт "Всегда загружать".
+    /// 
+    /// <list type="number">
+    /// <item>
+    /// <description>Особенности тестирования</description>
+    /// </item>
+    /// <item>
+    /// <description>AutoElement окно содержит только дочерние элементы.</description>
+    /// </item>
+    /// <item>
+    /// <description>Окна плагина принадлежат собственному процессу.</description>
+    /// </item>
+    /// </list>
+    /// 
+    /// </remarks>
     public class DrawCommandTests : IClassFixture<CadFixture>
     {
+        /// <summary>
+        /// Тестируемый процесс
+        /// </summary>
         private readonly Process _process;
 
+        /// <summary>
+        /// Инициализация тестов
+        /// </summary>
+        /// <param name="fixture">Менеджер жизни тестируемого процесса</param>
         public DrawCommandTests(CadFixture fixture)
-            =>  _process = fixture.Process;
-        
+            => _process = fixture.Process;
+
+        /// <summary>
+        /// Тестирование работы команды <b>MMP_DRAW</b>
+        /// </summary>
         [Fact]
         public void RunDrawCommand_ExecuteWithoutProblems()
         {
-            AutomationElement mainWindow = AutomationElement.FromHandle(_process.MainWindowHandle);
+            #region Инициализация главного окна AutoCAD
             
-            AutomationElement commandLine = mainWindow.FindFirst(TreeScope.Descendants,
-                new PropertyCondition(AutomationElement.AutomationIdProperty, "local:AutoCompleteEdit_1"));
+            const int sleepTime = 1_000;
 
-            if (commandLine != null)
-            {   
-                commandLine.SetFocus();
+            var mainWindow = AutomationElement.FromHandle(_process.MainWindowHandle);
 
-                SendKeys.SendWait("MMP_DRAW");
-                SendKeys.SendWait("{ENTER}");
-            }
+            var commandLineCondition = new PropertyCondition(AutomationElement.AutomationIdProperty, "local:AutoCompleteEdit_1");
+            mainWindow.FindFirst(TreeScope.Descendants, commandLineCondition).SendKeys("MMP_DRAW{ENTER}");
 
-            Thread.Sleep(1000);
+            Thread.Sleep(sleepTime);
 
-            var loginWindow = mainWindow.FindFirst(TreeScope.Descendants, new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Window));
+            #endregion
 
-            // TODO: Добавить поиск кнопок действий
+            #region Инициализация окна подключения
+
+            var wpfCondition = new PropertyCondition(AutomationElement.FrameworkIdProperty, "WPF");
+            var loginWindowCondition = new PropertyCondition(AutomationElement.AutomationIdProperty, "loginWindow");
+
+            var loginWindow = AutomationElement.RootElement.FindFirst(TreeScope.Descendants, new AndCondition(wpfCondition, loginWindowCondition));
+
+            loginWindow.FindFirst(TreeScope.Descendants, new PropertyCondition(AutomationElement.AutomationIdProperty, "usernamebox")).SendKeys("GEOUSER");
+            loginWindow.FindFirst(TreeScope.Descendants, new PropertyCondition(AutomationElement.AutomationIdProperty, "passwordBox")).SendKeys("GEOUSER");
+            loginWindow.FindFirst(TreeScope.Descendants, new PropertyCondition(AutomationElement.AutomationIdProperty, "host")).SendKeys("localhost");
+            loginWindow.FindFirst(TreeScope.Descendants, new PropertyCondition(AutomationElement.AutomationIdProperty, "dbName")).SendKeys("XEPDB1");
+            loginWindow.FindFirst(TreeScope.Descendants, new PropertyCondition(AutomationElement.AutomationIdProperty, "saveButton")).SendKeys("{ENTER}");
+
+            #endregion
+        }
+    }
+    /// <summary>
+    /// Класс расширения функциональности <see cref="AutomationElement"/>
+    /// </summary>
+    public static class AutomationElementExtensions
+    {
+        /// <summary>
+        /// Ввод информации в элемент управления
+        /// </summary>
+        /// <param name="element">Исходный элемент управления</param>
+        /// <param name="keys">Список клавиш для нажатия</param>
+        public static void SendKeys(this AutomationElement element, string keys)
+        {
+            element.SetFocus();
+
+            System.Windows.Forms.SendKeys.SendWait(keys);
         }
     }
 }
